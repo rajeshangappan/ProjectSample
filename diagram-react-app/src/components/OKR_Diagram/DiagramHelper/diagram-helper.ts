5bf61c1e0ce06a04e2cf0c740ffeea8a2d0417ce
livinkalai <livinjkt@gmail.com>
2021-05-25 09:01:07 -0700

    #8 - Node acting as port functionality is added.

--- diagram-react-app/src/components/OKR_Diagram/DiagramHelper/diagram-helper.ts
+++ diagram-react-app/src/components/OKR_Diagram/DiagramHelper/diagram-helper.ts
@@ -200,45 +200,28 @@ export class DiagramHelper {
     ];
     public static getNodeDefaults(node: NodeModel) {
         if ((node as Node).status != 'Update') {
-            node.ports = [DiagramHelper.leftMidPort, DiagramHelper.rightMidPort];
-            if (Util.IsNullOrUndefined(node.children)) {
-                node.width = 300;
-                node.height = 25;
-                node.shape = {
-                    type: 'Basic',
-                    shape: 'Rectangle',
-                    cornerRadius: 5
+            if (!this.IsDecoratorHelper(node.id)) {
+                if (Util.IsNullOrUndefined(node.children)) {
+                    node.width = 300;
+                    node.height = 25;
+                    node.shape = {
+                        type: 'Basic',
+                        shape: 'Rectangle',
+                        cornerRadius: 5
+                    }
                 }
+                node.style = nodeDefaultStyle;
+                for (let i: number = 0; i < node.annotations.length; i++)
+                    node.annotations[i].constraints = AnnotationConstraints.ReadOnly;
             }
-            // else {
+            node.ports = [DiagramHelper.leftMidPort, DiagramHelper.rightMidPort];
             node.constraints = NodeConstraints.Default & ~NodeConstraints.Select;
-            // }
-            node.style = nodeDefaultStyle;
-            for (let i: number = 0; i < node.annotations.length; i++)
-                node.annotations[i].constraints = AnnotationConstraints.ReadOnly;
         }
         return node;
     }
     public static getConnectorDefaults(connector: ConnectorModel) {
-        connector.sourceDecorator = {
-            shape: 'Square',
-            style: {
-                strokeWidth: 3,
-                fill : connectorDefaultStyle.strokeColor,
-                strokeColor: connectorDefaultStyle.strokeColor,
-                opacity: .2
-            }
-        }
-        connector.targetDecorator = {
-            shape: 'Custom',
-            pathData: 'M80.5,12.5 C80.5,19.127417 62.59139,24.5 40.5,24.5 C18.40861,24.5 0.5,19.127417 0.5,12.5' +
-                'C0.5,5.872583 18.40861,0.5 40.5,0.5 C62.59139,0.5 80.5,5.872583 80.5,12.5 z',
-            style: {
-                strokeWidth: 3,
-                strokeColor: 'green',
-                opacity: .2
-            },
-        }
+        connector.sourceDecorator.shape = 'None';
+        connector.targetDecorator.shape = 'None';
         connector.type = "Bezier";
         connector.style = connectorDefaultStyle;
         connector.sourcePortID = 'right-mid-port';
@@ -247,10 +230,7 @@ export class DiagramHelper {
         return connector;
     }
     public static onCreated() {
-        for (let i: number = 0; i < DiagramHelper.connectors.length; i++) {
-            //DiagramHelper.connectors[i].visible = false;
-            DiagramHelper.diagramInstance.add(DiagramHelper.connectors[i]);
-        }
+        this.CreateConnections();
     }
     public static onClick(args: IClickEventArgs) {
         if (args.actualObject instanceof Node) {
@@ -259,6 +239,64 @@ export class DiagramHelper {
                 this.SelectConnectedNodes(args.actualObject);
                 //this.diagramInstance.refreshDiagram();
             }
+        }
+    }
+    private static CreateConnections() {
+        for (let i: number = 0; i < this.connectors.length; i++) {
+            let link: ConnectorModel = this.connectors[i];
+            let srcNode: NodeModel = this.GetNode(link.sourceID);
+            let targetNode: NodeModel = this.GetNode(link.targetID);
+            let newlinkPrefix: string = link.sourceID + "_" + link.targetID + "_linkHelper"
+            let srcHelperNode: NodeModel = {
+                id: newlinkPrefix + "_node1",
+                width: 50,
+                height: 15,
+                style: {
+                    strokeColor: nodeDefaultStyle.strokeColor,
+                    fill: nodeDefaultStyle.strokeColor,
+                    strokeWidth: 0
+                },
+                offsetX: srcNode.offsetX + srcNode.width / 2 + 25,
+                offsetY: srcNode.offsetY
+            }
+            let targetHelperNode: NodeModel = {
+                id: newlinkPrefix + "_node2",
+                width: 50,
+                height: 15,
+                style: {
+                    strokeColor: nodeDefaultStyle.strokeColor,
+                    fill: nodeDefaultStyle.strokeColor,
+                    strokeWidth: 0
+                },
+                offsetX: targetNode.offsetX - targetNode.width / 2 - 25,
+                offsetY: targetNode.offsetY
+            }
+            this.diagramInstance.add(srcHelperNode);
+            this.diagramInstance.add(targetHelperNode);
+
+            let link1: ConnectorModel = {
+                sourceID: link.sourceID,
+                targetID: srcHelperNode.id,
+                id: newlinkPrefix + "_link1",
+                visible: false
+            }
+
+            let link2: ConnectorModel = {
+                sourceID: srcHelperNode.id,
+                targetID: targetHelperNode.id,
+                id: newlinkPrefix + "_link2",
+            }
+
+            let link3: ConnectorModel = {
+                sourceID: targetHelperNode.id,
+                targetID: link.targetID,
+                id: newlinkPrefix + "_link3",
+                visible: false
+            }
+
+            this.diagramInstance.add(link1);
+            this.diagramInstance.add(link2);
+            this.diagramInstance.add(link3);
         }
     }
     private static GetNode(nodeId: string): NodeModel {
@@ -278,12 +316,31 @@ export class DiagramHelper {
             }
         }
     }
+    private static IsDecoratorHelper(nodeId: string): boolean {
+        return nodeId.indexOf('_linkHelper') >= 0;
+    }
     private static SelectNode(nodeId: string) {
         this.SelectOutEdges(nodeId);
     }
     private static AddSelection(nodeModel: NodeModel) {
-        nodeModel.style.strokeColor = nodeSelectedStyle.strokeColor;
-        nodeModel.style.strokeWidth = nodeSelectedStyle.strokeWidth;
+        if (this.IsDecoratorHelper(nodeModel.id)) {
+            nodeModel.style.fill = nodeSelectedStyle.strokeColor;
+            nodeModel.style.strokeWidth = 0;
+        }
+        else {
+            nodeModel.style.strokeWidth = nodeSelectedStyle.strokeWidth;
+            nodeModel.style.strokeColor = nodeSelectedStyle.strokeColor;
+        }
+    }
+    private static ClearSelection(nodeModel: NodeModel) {
+        if (this.IsDecoratorHelper(nodeModel.id)) {
+            nodeModel.style.fill = nodeDefaultStyle.strokeColor;
+            nodeModel.style.strokeWidth = 0;
+        }
+        else {
+            nodeModel.style.strokeWidth = nodeDefaultStyle.strokeWidth;
+            nodeModel.style.strokeColor = nodeDefaultStyle.strokeColor;
+        }
     }
     private static SelectOutEdges(nodeId: string) {
         let nodeModel: NodeModel = this.GetNode(nodeId);
@@ -302,7 +359,7 @@ export class DiagramHelper {
         let nodes: NodeModel[] = this.diagramInstance.nodes;
         for (let i: number = 0; i < nodes.length; i++) {
             if (Util.IsNullOrUndefined(nodes[i].children)) {
-                nodes[i].style = nodeDefaultStyle;
+                this.ClearSelection(nodes[i]);
             }
         }
     }
